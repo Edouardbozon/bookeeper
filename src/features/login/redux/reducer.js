0bee2b5691b8c46eb905526e54d870d480fc9404
edouardbozon <bozon.edouard@live.fr>
2017-09-30 22:42:23 +0200

    Remove handle change action

--- src/features/login/redux/reducer.js
+++ src/features/login/redux/reducer.js
@@ -2,14 +2,12 @@ import initialState from './initialState';
 import { reducer as loginReducer } from './login';
 import { reducer as logoutReducer } from './logout';
 import { reducer as toggleSignupReducer } from './toggleSignup';
-import { reducer as handleChangeReducer } from './handleChange';
 import { reducer as signupReducer } from './signup';
 
 const reducers = [
   loginReducer,
   logoutReducer,
   toggleSignupReducer,
-  handleChangeReducer,
   signupReducer,
 ];
 
